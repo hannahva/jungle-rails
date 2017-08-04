aedb4c064f66c3e9411cccef546bb1f2ac668579
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-04 21:04:46 +0000

    adds basic HTTP auth to admin controllers

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
     @category = Category.order(created_at: :desc).all
 
