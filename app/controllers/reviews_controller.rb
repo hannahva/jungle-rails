f15e27d307404dd84944d53314fbae27ec040c72
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-05 00:29:47 +0000

    adds create method to reviews controller, can submit reviews via form on products' pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save!
+      redirect_to @product
+    else
+      redirect_to @product , notice: "something went wrong!"
+    end
 
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+      )
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :product
-  validates :user, :product, :description, :rating, presence: true
+  validates :user,
+            :product,
+            :description,
+            :rating, presence: true
 end
