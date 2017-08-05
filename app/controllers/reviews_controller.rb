5b4185edd946718fb1c7f22270ad116df5122a96
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-08-05 01:24:03 +0000

    only logged in users can leave reviews. if review author same as current_user, can delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+
+  before_filter :authorize
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
@@ -8,7 +11,13 @@ class ReviewsController < ApplicationController
     else
       redirect_to @product , notice: "something went wrong!"
     end
+  end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to @product
   end
 
   private
